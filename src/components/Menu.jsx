e1bbec7d51b9e47250ff7a88d1b1a272afe2b565
Станислав Дмитриевич Кирчанов <19300517@live.inueco.ru>
2021-05-13 10:52:07 +0500

    Somthing that work?

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 
 export const Menu = (props) => {
-    //TODO проверка данных - Заменить курс 1,2,3 на ссылки из CorsesList.jsx
+    //TODO проверка данных - Заменить курсы с ссылками на них 1,2,3 на ссылки из CorsesList.jsx
    
     const entries = props.entries || [];
     return (
         <nav>
         <ul>
-          <li><a href="">Главная</a></li> 
+          <li><a href="http://localhost:1234">Главная</a></li> 
           <li> 
               <div class="dropdown">
-                  <button onclick="RedTeamFunction()"  class="dropbtn">Курсы</button>
+                  <button onСlick="RedTeamFunction()"  className="dropbtn"><a href="http://localhost:1234/coursesList">Курсы</a></button>
                   <div id="courses" class="dropdown-content">
-                    <a href="">Курс 1</a>
-                    <a href="">Курс 2</a> 
-                    <a href="">Курс 3</a> 
+                    <a href="http://localhost:1234/coursesList/course1">Курс 1</a>
+                    <a href="http://localhost:1234/coursesList/course2">Курс 2</a> 
+                    <a href="http://localhost:1234/coursesList/course3">Курс 3</a> 
                   </div>
               </div>
           </li>
